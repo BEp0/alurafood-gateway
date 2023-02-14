9141764ccf9f04f7f41588066e6998d69a99733a
Bernardo Porto e Silva Souza <bernardo.souza@cwi.com.br>
2023-02-14 19:25:10 -0300

    Initial commit

---
+++ src/main/java/br/com/alurafood/gateway/AlurafoodGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.alurafood.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlurafoodGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlurafoodGatewayApplication.class, args);
+	}
+
+}
